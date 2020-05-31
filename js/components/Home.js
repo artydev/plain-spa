3ac85396c6a07c5d272528faf6ce0c732f776fa0
DI DIO Salvatore (SICCRF) <sdidio-ccrf@ccrf.si.mfi>
2020-05-31 16:39:02 +0200

    allydx

--- js/components/Home.js
+++ js/components/Home.js
@@ -1,7 +1,7 @@
 import { html } from '../modules/sinuous/index.js'
 
 const HelloMessage = ({ name }) => html`
-  <h1 class="title text-4">Hello SPA</h1>
+  <h1 class="title text-4">Hello SPAX</h1>
 `
 
 export const Home = () => html`
